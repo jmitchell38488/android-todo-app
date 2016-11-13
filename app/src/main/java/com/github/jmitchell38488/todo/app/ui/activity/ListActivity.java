ae06e2aa17e6b8109048834c1d93b03dad68c1f8
Justin Mitchell <jmitchell38488@gmail.com>
2016-11-14 08:07:06 +1100

    Adding ActionMenuView back to ListActivity

--- app/src/main/java/com/github/jmitchell38488/todo/app/ui/activity/ListActivity.java
+++ app/src/main/java/com/github/jmitchell38488/todo/app/ui/activity/ListActivity.java
@@ -7,7 +7,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -34,6 +39,8 @@ public class ListActivity extends AppCompatActivity implements TodoItemDialogLis
     @Inject SharedPreferences prefs;
     @BindView(R.id.fab) FloatingActionButton fab;
 
+    private ActionMenuView amvMenu;
+
     private ListFragment mFragment;
 
     private final static String COUNTER = "counter";
@@ -42,6 +49,7 @@ public class ListActivity extends AppCompatActivity implements TodoItemDialogLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        setMenuBar();
 
         TodoApp.getComponent(this).inject(this);
 
@@ -67,6 +75,36 @@ public class ListActivity extends AppCompatActivity implements TodoItemDialogLis
                 showEditDialog(arguments);
             }
         });
+    }
+
+    private void setMenuBar() {
+        Toolbar t = (Toolbar) findViewById(R.id.list_toolbar);
+        amvMenu = (ActionMenuView) t.findViewById(R.id.amvMenu);
+
+        amvMenu.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onOptionsItemSelected(menuItem);
+            }
+        });
+
+        setSupportActionBar(t);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // use amvMenu here
+        inflater.inflate(R.menu.list, amvMenu.getMenu());
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Do your actions here
+        return true;
     }
 
     public void showEditDialog(Bundle arguments) {
