024209960e6c1eea10a8695a9bfedfe4c1fba67c
Justin Mitchell <jmitchell38488@gmail.com>
2016-11-19 19:24:12 +1100

    Removed the dialog code and layouts

--- app/src/main/java/com/github/jmitchell38488/todo/app/ui/dialog/TodoItemDialogListener.java
+++
@@ -1,8 +0,0 @@
-package com.github.jmitchell38488.todo.app.ui.dialog;
-
-import android.support.v4.app.DialogFragment;
-
-public interface TodoItemDialogListener {
-    public void onDialogPositiveClick(DialogFragment dialog);
-    public void onDialogNegativeClick(DialogFragment dialog);
-}
